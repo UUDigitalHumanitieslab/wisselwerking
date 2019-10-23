171154bd609d2ec30f0c4b1fbdfae8273456321e
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-10-23 15:28:44 +0200

    Generate mail

--- magic.py
+++ magic.py
@@ -12,17 +12,56 @@ ASSIGNED_CHOICE = "toegewezen"
 CAPACITY_CHOICE = "keuze"
 CAPACITY_VALUE = "aantal"
 
+TEAM = "Desiree Capel, Nicoline Fokkema, Yvonne de Jong en Sheean Spoel"
+MAIL_COLUMN = "mail"
+
 ENROLLMENT_NAME = "naam"
 ENROLLMENT_PHONE = "telefoonnummer"
 ENROLLMENT_CHOICES = ["eerste_keuze", "tweede_keus", "derde_keus"]
 
 NONE_CHOICE = "Maak je keuze"
 
+#
+# Start assigning!
+#
+
 enrollments = []
 capacities = {}
 counter = {}
 
 assignments = []
+
+def format_name(enrollment):
+    name = enrollment[ENROLLMENT_NAME]
+    return name.strip().split(' ')[0].capitalize()
+
+def mail_template(assigned, enrollment):
+    name = format_name(enrollment)
+    first_choice = enrollment[ENROLLMENT_CHOICES[0]].strip()
+    second_choice = enrollment[ENROLLMENT_CHOICES[1]].strip()
+    if assigned == first_choice:
+        # first choice
+        explain_choice = f"de Wisselwerking {assigned}. Het doet ons plezier je te laten weten dat je geplaatst bent voor deze Wisselwerking"
+    elif assigned == NONE_CHOICE:
+        # nothing
+        explain_choice = f"de Wisselwerking {assigned}. Helaas waren deze en eventuele verdere keuzes vol"
+    else:
+        # second, third choice
+        ordinal = "tweede" if assigned == second_choice else "derde"
+        explain_choice = f"de Wisselwerking. Helaas was bij jouw eerste keuze {first_choice} geen plek meer. Je bent nu geplaatst bij je {ordinal} keuze: {assigned}"
+    
+    return f"""
+Beste {name},
+
+Je hebt je aangemeld voor {explain_choice}.
+
+We hebben je gegevens doorgegeven aan de contactpersoon van jouw Wisselwerking. Hij of zij neemt contact met je op om verdere afspraken te maken over je deelname.
+
+Hartelijke groet,
+
+Team Wisselwerking Geesteswetenschappen
+{TEAM}
+""".strip()
 
 if os.path.exists(capacity_file):
     with open(capacity_file, mode="r", encoding="utf-8-sig") as csv_file:
@@ -110,12 +149,13 @@ for choice in sorted(counter):
 
 # Store the assignments
 with open(output_file, mode="w", encoding="utf-8-sig") as csv_file:
-    fieldnames = [ASSIGNED_CHOICE] + form_fieldnames
+    fieldnames = [ASSIGNED_CHOICE, MAIL_COLUMN] + form_fieldnames
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=';')
 
     writer.writeheader()
     for (row, assigned) in assignments:
         writer.writerow({
             ASSIGNED_CHOICE: assigned,
+            "mail": mail_template(assigned, row),
             **row
         })
